6bd6bea1ab57508b1ec5458122c49d97a115df68
KelvinNatal <kelvinnatal@hotmail.com>
2022-07-02 16:15:18 -0300

    Final

--- solution.js
+++ solution.js
@@ -8,17 +8,18 @@ terminal: false
     rl.setPrompt("Digite a palavra:");
     rl.prompt();
     rl.on('line',  entrada => {                                       
-      var resposta ='';
-      var resposta1 = '';
-      var palavras = entrada.split(' ')
-    for(var i = 0; i<palavras.length; i++){
-      resposta = palavras[i].replace(/(.)(?=.*\1)/g, '');
-      if(resposta === palavras[i]){
-           resposta1 = entrada;      
+      let resposta ='';
+      let resposta1 = '';
+      let palavras = entrada.split(' ')
+    for(let i = 0; i<palavras.length; i++){
+      resposta1 = palavras[i].replace(/(.)(?=.*\1)/g, '');
+      if(resposta1 === palavras[i]){
+           resposta = entrada;      
            break;          
       }else{
-           resposta1 += palavras[i].replace(/(.)(?=.*\1)/g, '') + " ";
+          resposta += palavras[i].replace(/(.)(?=.*\1)/g, '') + " ";
+          
       }
-    } 
-    console.log(resposta1 + ".");
+    }
+    console.log(resposta);
     })
